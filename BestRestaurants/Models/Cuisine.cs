19b2f3ace85ab4efd2c4c9a0bf387f0bef2b93da
Brian Scherner <brianscherner@gmail.com>
2023-10-23 11:57:55 -0700

    Build classes and add functionality to view Home page.

---
+++ BestRestaurants/Models/Cuisine.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace BestRestaurants.Models
+{
+
+  public class Cuisine
+  {
+
+    public int CuisineId { get; set; }
+
+    public string Type { get; set; }
+
+    public List<Restaurant> Restaurants { get; set; }
+
+  }
+}
---
+++ BestRestaurants/Models/Restaurant.cs
@@ -0,0 +1,16 @@
+namespace BestRestaurants.Models
+{
+
+  public class Restaurant
+  {
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public int CuisineId { get; set; }
+
+    public Cuisine Cuisine { get; set; }
+
+  }
+}
