67a509cfdcea579322bad648cdd7f10a491ebe98
Brian Scherner <brianscherner@gmail.com>
2023-10-24 10:17:01 -0700

    Add functionality to view Cuisine details w/ route.

--- BestRestaurants/Controllers/CuisinesController.cs
+++ BestRestaurants/Controllers/CuisinesController.cs
@@ -35,5 +35,11 @@ namespace BestRestaurants.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      Cuisine thisCuisine = _db.Cuisines.Include(cuisine => cuisine.Restaurants).FirstOrDefault(cuisine => cuisine.CuisineId == id);
+      return View(thisCuisine);
+    }
   }
 }
