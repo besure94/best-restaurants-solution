b4e8c4d0334169db643390ed04c878054b573534
Brian Scherner <brianscherner@gmail.com>
2023-10-24 09:36:14 -0700

    Add functionality to view Index for restaurants w/ matching route.

---
+++ BestRestaurants/Controllers/RestaurantsController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using BestRestaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BestRestaurants.Controllers
+{
+  public class RestaurantsController : Controller
+  {
+    private readonly BestRestaurantsContext _db;
+    public RestaurantsController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Restaurant> model = _db.Restaurants.Include(restaurant => restaurant.Cuisine).ToList();
+      ViewBag.PageTitle = "View all Restaurants";
+      return View(model);
+    }
+  }
+}
--- BestRestaurants/Models/Restaurant.cs
+++ BestRestaurants/Models/Restaurant.cs
@@ -10,6 +10,8 @@ namespace BestRestaurants.Models
 
     public int CuisineId { get; set; }
 
+    public int RestaurantId { get; set; }
+
     public Cuisine Cuisine { get; set; }
 
   }
